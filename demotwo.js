7e9d636738df7a76ea45d319003cb1b01abc48c5
Remsey Mailjard <remsey@skills4-it.nl>
2024-05-13 14:54:16 -0500

    UpdateIT

---
+++ demotwo.js
@@ -0,0 +1,8 @@
+let gifts = [
+    { idea: "A house", value: "Gift1" },
+    { idea: "A vacation", value: "Gift2" },
+    { idea: "A rose", value: "Gift3" },
+    { idea: "A photoalbum", value: "Gift4" }
+]
+
+console.log(gifts[0].value);
